99775d4eb887a98bb02521311b592d05b941f15c
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-04-22 12:12:01 +0100

    Fixed synchronization issue
    Between the carousel timer and the dots by refactoring my code, the showSlides() function now clears the timeout (clearTimeout(timeOutId)) and then schedules a new timeout (timeOutId = setTimeout(...)) to trigger the showSlides() function again after 2 seconds. This ensures that the slides move by themselves when there is no interaction with the slide.

--- libs/script.js
+++ libs/script.js
@@ -9,57 +9,52 @@ function Hamburger(nodeName) {
         (e) =>
           myNode.parentNode.querySelector(".navbar").classList.toggle("hidden"),
         false
-      ), //activate
+      ) //activate
   }; //return
 } //Hamburger
 
 let topMenu = new Hamburger("#topMenu");
 topMenu.activate();
 
-//Icon slider
 ("use strict");
 var slideIndex = 1;
 showSlides(slideIndex);
+
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
+
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
 var timeOutId;
-// showSlides(slideIndex);
-function plusDivs(n) {
-  showSlides((slideIndex += n));
-}
-
-var timer;
-function showSlides(n) {
-  /* the code */
-  if (timer) clearTimeout(timer);
-  timer = setTimeout(showSlides, 8000);
-}
 
 function showSlides(n) {
   // Stop the timeout from triggering.
   clearTimeout(timeOutId);
+
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+
   if (n == undefined) {
     n = ++slideIndex;
   }
@@ -74,9 +69,13 @@ function showSlides(n) {
   }
 
   slides[slideIndex - 1].style.display = "block";
+
   // Schedule a new timeout.
-  timeOutId = setTimeout(showSlides, 2000); // Change image every 2 seconds
+  timeOutId = setTimeout(function () {
+    showSlides((slideIndex += 1));
+  }, 2000); // Change image every 2 seconds
 }
+
 //Pricing Per Build
 let selectPlan = document.querySelector("#dropdown");
 let basicBtn = document.querySelector("#requestbasic");
